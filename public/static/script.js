a1a81d34eabbf9f0c95e14d42922beccf348d635
fnassar <fan6236@nyu.edu>
2023-05-25 01:54:57 +0300

    aaaaaaaa nodejs now

---
+++ public/static/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+    if(!form.checkValidity()) {
+        e.preventDefault();
+        // alert("Please fill out the form!");
+        // document.getElementById('alert').style.display = "block";
+        // document.getElementById('alert').innerHTML = "Please fill out the form!";
+        // return false;
+    }
+    form.classList.add('was-validated');
+});
