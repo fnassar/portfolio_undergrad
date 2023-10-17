f91f10e327a89024f1f01dc420feaf57fa8d680e
fnassar <fan6236@nyu.edu>
2023-10-17 16:21:40 +0400

    .

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,9 @@
 // frontend/src/App.js
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Cursor } from 'react-creative-cursor';
-import 'react-creative-cursor/dist/styles.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Cursor } from "react-creative-cursor";
+import "react-creative-cursor/dist/styles.css";
 import "./App.css";
-
 
 // import Opening from "./pages/Opening.js";
 import Backg from "./pages/Backg.js";
@@ -12,17 +11,22 @@ import Home from "./pages/Home.js";
 import Projectss from "./pages/Projectss.js";
 import Nav from "./pages/Nav.js";
 import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
 const App = () => {
     return (
         <div className="blue-bg">
-            <Cursor isGelly={true} cursorBackgrounColor={"hsla(245, 30%, 90%, 50%)"}/>
+            <Cursor
+                isGelly={true}
+                cursorBackgrounColor={"hsla(245, 30%, 90%, 50%)"}
+            />
             {/* <CostumeCursor key="x0" /> */}
             <Backg key="x1" />
-            <Nav key="x2"/>
+            <Nav key="x2" />
             {/* <Opening key="x1" /> */}
             <Home key="x3" />
             <About key="x4" />
             <Projectss key="x5" />
+            <Contact key="x6" />
         </div>
     );
 };
