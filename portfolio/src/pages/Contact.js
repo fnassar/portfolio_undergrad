cae671e59e2cbbbcde20da7cbc09bf6a4cc08ece
fnassar <fan6236@nyu.edu>
2023-10-18 17:37:08 +0400

    k

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -33,64 +33,64 @@ class Contact extends React.Component {
     }
     render() {
         return (
-            // <div className="App" id="about">
-            //     <div className="container mt-5" style={{ minHeight: "90vh" }}>
-            //         <div className="row mb-5 d-flex justify-content-center">
-            //             <div className="col-12">
-            //                 <h1 className="f-1">Contact me</h1>
-            //             </div>
-            //             <div className="col-9 m-5 p-5">
-            //                 <form
-            //                     id="contact-form"
-            //                     onSubmit={this.handleSubmit.bind(this)}
-            //                     method="POST"
-            //                 >
-            //                     <div className="form-group">
-            //                         <label htmlFor="name">Name</label>
-            //                         <input
-            //                             type="text"
-            //                             className="form-control"
-            //                             id="name"
-            //                             value={this.state.name}
-            //                             onChange={this.onNameChange.bind(this)}
-            //                         />
-            //                     </div>
-            //                     <div className="form-group">
-            //                         <label htmlFor="exampleInputEmail1">
-            //                             Email address
-            //                         </label>
-            //                         <input
-            //                             type="email"
-            //                             className="form-control"
-            //                             id="email"
-            //                             aria-describedby="emailHelp"
-            //                             value={this.state.email}
-            //                             onChange={this.onEmailChange.bind(this)}
-            //                         />
-            //                     </div>
-            //                     <div className="form-group">
-            //                         <label htmlFor="message">Message</label>
-            //                         <textarea
-            //                             className="form-control"
-            //                             rows="5"
-            //                             id="message"
-            //                             value={this.state.message}
-            //                             onChange={this.onMessageChange.bind(
-            //                                 this
-            //                             )}
-            //                         />
-            //                     </div>
-            //                     <button
-            //                         type="submit"
-            //                         className="btn btn-primary"
-            //                     >
-            //                         Submit
-            //                     </button>
-            //                 </form>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div>
+            <div className="App" id="about">
+                {/* <div className="container mt-5" style={{ minHeight: "90vh" }}>
+                    <div className="row mb-5 d-flex justify-content-center">
+                        <div className="col-12">
+                            <h1 className="f-1">Contact me</h1>
+                        </div>
+                        <div className="col-9 m-5 p-5">
+                            {/* <form
+                                id="contact-form"
+                                onSubmit={this.handleSubmit.bind(this)}
+                                method="POST"
+                            >
+                                <div className="form-group">
+                                    <label htmlFor="name">Name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        value={this.state.name}
+                                        onChange={this.onNameChange.bind(this)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">
+                                        Email address
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        aria-describedby="emailHelp"
+                                        value={this.state.email}
+                                        onChange={this.onEmailChange.bind(this)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="message">Message</label>
+                                    <textarea
+                                        className="form-control"
+                                        rows="5"
+                                        id="message"
+                                        value={this.state.message}
+                                        onChange={this.onMessageChange.bind(
+                                            this
+                                         )}
+                                     />
+                                 </div>
+                                 <button
+                                     type="submit"
+                                     className="btn btn-primary"
+                                 >
+                                     Submit
+                                 </button>
+                             </form>
+                         </div>
+                     </div>
+                 </div> */}
+            </div>
         );
     }
     onNameChange(event) {
