1186e2c626637b9cbd0c568d8e36017fcfe2bd36
fnassar <fan6236@nyu.edu>
2023-10-19 01:39:54 +0400

    d

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -8,49 +8,50 @@ const Home = () => {
         <div className="App mt-5 zIndex-2" id="home">
             <div
                 className="container-fluid d-flex justify-content-center align-items-center mt-5"
-                style={{ minHeight: "90vh" }}
+                style={{ minHeight: "90vh", maxHeight: "90vh !important" }}
             >
                 <div className="row mb-5">
                     <div className="col-12">
-                        <h1 className="mb-0">
+                        {/* wrap text for phone size */}
+                        <h1 className="mb-0 d-flex flex-wrap justify-content-center col-8 col-md-10">
                             <span
+                                className="text-left"
                                 style={{
                                     color: "hsl(245, 30%, 90%)",
                                     fontSize: "calc(3rem + 4vw)",
-                                    lineHeight: "0.7",
+                                    lineHeight: "1",
                                 }}
                             >
                                 Fatema
                             </span>
                             <span
+                                className="text-left"
                                 style={{
                                     color: "hsl(245, 50%, 70%)",
                                     fontSize: "calc(3rem + 4vw)",
-                                    lineHeight: "0.7",
+                                    lineHeight: "1",
                                 }}
                             >
                                 Nassar
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                             </span>
                         </h1>
                     </div>
-                    <div className="col-12">
-                        <h1 className="mb-5">
+                    <div className="col-10">
+                        <h1 className="mb-5 d-flex flex-wrap justify-content-end">
                             <span
                                 style={{
                                     color: "hsl(245, 30%, 90%)",
                                     fontSize: "calc(3rem + 4vw)",
-                                    lineHeight: "0.8",
+                                    lineHeight: "1",
                                 }}
                             >
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                 فاطمة
                             </span>
                             <span
                                 style={{
                                     color: "hsl(245, 50%, 70%)",
                                     fontSize: "calc(3rem + 4vw)",
-                                    lineHeight: "0.8",
+                                    lineHeight: "1",
                                 }}
                             >
                                 نصار
@@ -58,42 +59,27 @@ const Home = () => {
                         </h1>
                     </div>
                     <div className="col-12">
-                        <h3>
+                        {/* <h3 className="mb-0 d-flex flex-wrap justify-content-center col-10 col-md-10"> */}
+                        <h3 className="mb-0 d-flex flex-wrap justify-content-center col-9 col-md-10 px-5 px-md-0">
                             <span
+                                className="text-align-left pr-4 pr-md-2"
                                 style={{
                                     fontSize: "calc(1.2rem + 1vw)",
                                 }}
                             >
-                                Computer Science & Interactive Media
-                            </span>
-                            <span
-                                style={{
-                                    color: "hsl(245, 50%, 70%)",
-                                    fontSize: "calc(1.2rem + 1vw)",
-                                }}
-                            >
-                                &nbsp;Student
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                Computer Science & Interactive Media Student
                             </span>
                         </h3>
                     </div>
-                    <div className="col-12">
-                        <h3 className="text-light">
+                    <div className="col-10">
+                        <h3 className="mb-5 d-flex flex-wrap justify-content-end text-align-right text-light col-12 col-md-10">
                             <span
-                                style={{
-                                    color: "hsl(245, 50%, 70%)",
-                                    fontSize: "calc(1.2rem + 1vw)",
-                                }}
-                            >
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                طالبة&nbsp;
-                            </span>
-                            <span
+                                className="text-align-right"
                                 style={{
                                     fontSize: "calc(1.2rem + 1vw)",
                                 }}
                             >
-                                حاسبات و معلومات و فنون تفاعلية
+                                طالبة حاسبات و معلومات و فنون تفاعلية
                             </span>
                         </h3>
                     </div>
