09334dc86fd94149b94730d3a615d5a5b2f52eac
Fatema Nassar <75119980+fnassar@users.noreply.github.com>
2024-03-21 14:34:04 +0400

    Update Nav.js

--- portfolio/src/pages/Nav.js
+++ portfolio/src/pages/Nav.js
@@ -42,7 +42,7 @@ const Nav = () => {
                         <li className="nav-item">
                             <a
                                 className="nav-link text-light font-weight-bolder hover-zoom"
-                                href="https://drive.google.com/file/d/1W1hvwFsW_kwleNBmApUTD64d-xaQDieV/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1iItYbWa4QHgcnQNuXiMfz-FL5sA1_3tg/view?usp=sharing"
                                 target="_blank"
                                 rel="noreferrer"
                             >
