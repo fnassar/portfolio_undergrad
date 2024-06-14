f78a88309d030b6e1f3467bcf4e75313b5310d30
Fatema Nassar <75119980+fnassar@users.noreply.github.com>
2024-06-14 14:15:51 +0300

    again Update About.js

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -19,8 +19,8 @@ const About = () => {
                                     fontSize: "calc(0.6rem + 0.6vw)",
                                 }}
                             >
-                                What you are seeing now is my portfolio, The work of the last 4+ years of my life, and explanations on a lot of what I did, my interests and experiences, my passions, and things I care about. <br>
-                                First, My introduction is that I am a passionate programmer, I love problem-solving and finding ways to do things I did not previously know I could do, and I probably like debugging more than I should. Besides that, however, I also have an inner artist in me that I cannot seem to suppress. I found the interactive media major in my undergrad to be an outlet for my artistic self, a way to express, create, and show a part of me one cannot see in a program with a set output requirement. <br>
+                                What you are seeing now is my portfolio, The work of the last 4+ years of my life, and explanations on a lot of what I did, my interests and experiences, my passions, and things I care about. <br></br>
+                                First, My introduction is that I am a passionate programmer, I love problem-solving and finding ways to do things I did not previously know I could do, and I probably like debugging more than I should. Besides that, however, I also have an inner artist in me that I cannot seem to suppress. I found the interactive media major in my undergrad to be an outlet for my artistic self, a way to express, create, and show a part of me one cannot see in a program with a set output requirement. <br></br>
                                 There is a lot more to anyone than meets the eye. Still, talking about myself, I know that breaking an algorithm with a group that interacts, learning from one another, and finding new tools while developing new connections is how I wish to spend the next couple years of my life.
                             </span>
                         </p>
